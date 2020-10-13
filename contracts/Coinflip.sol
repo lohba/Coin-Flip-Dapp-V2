4c4e7553cd8b0e407ab1f59d6e0fd787fc2a0aff
Luke Ohba <lukeohba@gmail.com>
2020-10-12 23:17:00 -0400

    edits

--- contracts/Coinflip.sol
+++ contracts/Coinflip.sol
@@ -9,8 +9,68 @@ contract Coinflip is Ownable, usingProvable {
     event LogNewProvableQuery(string description);
     event generatedRandomNumber(uint256 randomNumber);
 
-    constructor() public {
-        update();
+    // constructor() public {
+    //     update();
+    // }
+    
+    mapping(address => bool) public bettings;
+    mapping(bytes32 => Bet) public waiting;
+    mapping(address => uint) private playerBalances;
+   
+    uint256 public balance;
+
+      struct Bet {
+        address player;
+        uint256 betValue;
+        bool heads;
+        bool won;
+    }
+
+    // function update() public payable {
+    //     uint256 QUERY_EXECUTION_DELAY = 0;
+    //     uint256 GAS_FOR_CALLBACK = 200000;
+    //     bytes32 queryId = provable_newRandomDSQuery(
+    //         QUERY_EXECUTION_DELAY,
+    //         NUM_RANDOM_BYTES_REQUESTED,
+    //         GAS_FOR_CALLBACK
+    //     );
+    //     waiting[queryId] = newBet;
+    //     emit LogNewProvableQuery(
+    //         "Provable query was sent, standing by for the answer..."
+    //     );
+    // }
+
+    // Old Random Function
+    // function random() public view returns (bool) {
+    //     return (now % 2 != 0);
+    // }
+
+    function flipCoin(bool bet) public payable {
+        require(msg.value <= balance);
+        Bet memory newBet;
+        newBet.heads = bet;
+        //player pays fee for calling oracle
+        newBet.betValue = msg.value - provable_getPrice("RANDOM");
+        newBet.player = msg.sender;
+        // Random Function
+        uint256 QUERY_EXECUTION_DELAY = 0;
+        uint256 GAS_FOR_CALLBACK = 200000;
+        bytes32 queryId = provable_newRandomDSQuery(
+            QUERY_EXECUTION_DELAY,
+            NUM_RANDOM_BYTES_REQUESTED,
+            GAS_FOR_CALLBACK
+        );
+        waiting[queryId] = newBet;
+
+        //bool headsOrTails = random();
+        // if (headsOrTails == bet) {
+        //     balance = balance - (msg.value);
+        //     msg.sender.transfer((msg.value * 2));
+        //     bettings[msg.sender] = true;
+        // } else {
+        //     balance = balance + msg.value;
+        //     bettings[msg.sender] = false;
+        // }
     }
 
     function __callback(
@@ -19,48 +79,13 @@ contract Coinflip is Ownable, usingProvable {
         bytes memory _proof
     ) public {
         require(msg.sender == provable_cbAddress());
-        uint256 randomNumber = uint256(keccak256(abi.encodePacked(_result))) %
-            2;
-        latestNumber = randomNumber;
-        emit generatedRandomNumber(randomNumber);
-    }
-
-    function update() public payable {
-        uint256 QUERY_EXECUTION_DELAY = 0;
-        uint256 GAS_FOR_CALLBACK = 200000;
-        bytes32 queryId = provable_newRandomDSQuery(
-            QUERY_EXECUTION_DELAY,
-            NUM_RANDOM_BYTES_REQUESTED,
-            GAS_FOR_CALLBACK
-        );
-        emit LogNewProvableQuery(
-            "Provable query was sent, standing by for the answer..."
-        );
-    }
-
-    struct Bet {
-        address player;
-        uint256 betValue;
-    }
-
-    mapping(address => bool) public bettings;
-    mapping(uint256 => Bet) public waiting;
-    uint256 public balance;
-
-    function random() public view returns (bool) {
-        return (now % 2 != 0);
-    }
-
-    function flipCoin(bool bet) public payable {
-        bool headsOrTails = random();
-        if (headsOrTails == bet) {
-            balance = balance - (msg.value);
-            msg.sender.transfer((msg.value * 2));
-            bettings[msg.sender] = true;
-        } else {
-            balance = balance + msg.value;
-            bettings[msg.sender] = false;
+        bool result = (uint256(keccak256(abi.encodePacked(_result))) % 2) != 0;
+        if(result = waiting[_queryId].heads){
+            waiting[_queryId].won = true;
+            balance = balance - (waiting[_queryId].betValue);
+            playerBalances[]
         }
+        emit generatedRandomNumber(randomNumber);
     }
 
     function deposit() public payable {
